2d6cc928f2268b1ec8e815fc8b78d1e78eeb69a5
Sal Meza <salmeza80@gmail.com>
2026-09-02 18:07:40 -0400

    a recreation of the python list class

---
+++ custom_list_class.py
@@ -0,0 +1,26 @@
+import ctypes
+
+class CustomList:
+    def __init__(self):
+        initialCapacity = 1
+        self.capacity = initialCapacity
+        self.size = 0
+        self.array = self.__create_array(self.capacity)
+
+    def __create_array(self, capacity):
+        '''Creates a new referential array with the given capacity'''
+        return (capacity * ctypes.py_object)()
+
+    def __resize(self, new_capacity):
+        new_array = self.__create_array(new_capacity)
+        for i in range(self.size):
+            new_array[i] = self.array[i]
+
+        self.array = new_array
+        self.capacity = new_capacity
+
+    def append(self, item):
+        pass
+
+    def __len__(self):
+        pass
